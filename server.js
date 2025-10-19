3e03b6910c73928924886bb7e6bc08de9d5cae1c
Anthony Muhoro <113031871+Anthony-muhoro@users.noreply.github.com>
2025-10-19 17:58:01 +0300

    Implement greeting route in server.js
    Add a new route that returns a greeting message.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+app.get('/',(rq,res)=>{
+  message:"Hello from Anthony"}
 
 // Email sender setup
 const transporter = nodemailer.createTransport({
